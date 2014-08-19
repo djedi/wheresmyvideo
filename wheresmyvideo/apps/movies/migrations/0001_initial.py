38b843d08d77d6459a085690ac751692003721b3
Dustin Davis <dustin@davis.im>
2014-08-19 08:47:00 -0600

    squash migrations

--- wheresmyvideo/apps/movies/migrations/0001_initial.py
+++ wheresmyvideo/apps/movies/migrations/0001_initial.py
@@ -13,10 +13,22 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Genre',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30)),
+                ('tmdb_id', models.PositiveSmallIntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='MediaType',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=100)),
+                ('users', models.ManyToManyField(to=settings.AUTH_USER_MODEL, null=True, blank=True)),
             ],
             options={
             },
@@ -26,9 +38,26 @@ class Migration(migrations.Migration):
             name='Movie',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(null=True, blank=True)),
+                ('modified_at', models.DateTimeField(null=True, blank=True)),
                 ('title', models.CharField(max_length=255)),
-                ('tmdb_id', models.PositiveIntegerField(null=True, blank=True)),
-                ('media_types', models.ManyToManyField(to='movies.MediaType')),
+                ('release_date', models.DateField(null=True, blank=True)),
+                ('tmdb_id', models.PositiveIntegerField(unique=True)),
+                ('rating', models.CharField(max_length=5, null=True, blank=True)),
+                ('tmdb_poster', models.CharField(max_length=255, null=True, blank=True)),
+                ('genres', models.ManyToManyField(to='movies.Genre', null=True, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserMovie',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('media_types', models.ManyToManyField(to='movies.MediaType', null=True, blank=True)),
+                ('movie', models.ForeignKey(to='movies.Movie')),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={

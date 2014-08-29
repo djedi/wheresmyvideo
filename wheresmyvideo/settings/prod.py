7207d0f7560efd73506c0130cbccb2660c8e19db
Dustin Davis <dustin@davis.im>
2014-08-29 14:25:29 -0600

    add logging setting

--- wheresmyvideo/settings/prod.py
+++ wheresmyvideo/settings/prod.py
@@ -22,3 +22,46 @@ RAVEN_CONFIG = {
     'dsn': 'http://14841332d1f24d79b03c12d2fb65cc70:1a5077a523c244048341381ec20'
            '8a5ed@redseam.info/10',
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(threa'
+                      'd)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}

e6adf88387f5eeafd2b8f6811ce17e10ca9ce5b7
Dustin Davis <dustin@davis.im>
2014-08-29 12:03:41 -0600

    prod settings

---
+++ wheresmyvideo/settings/prod.py
@@ -0,0 +1,13 @@
+import os
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USERNAME'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}

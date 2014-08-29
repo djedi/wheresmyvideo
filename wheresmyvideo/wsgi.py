424f003caf160468f56e99b131f0bd3972a2df91
Dustin Davis <dustin@davis.im>
2014-08-29 15:52:10 -0600

    try again

--- wheresmyvideo/wsgi.py
+++ wheresmyvideo/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wheresmyvideo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wheresmyvideo.settings.prod")
 
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 from django.core.wsgi import get_wsgi_application
